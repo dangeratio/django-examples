47b49be849d19567cd0be1600d57daa8d73ba9a6
dangeratio <dangeratio@gmail.com>
2015-04-17 21:33:00 -0700

    Updates

--- article/views.py
+++ article/views.py
@@ -1,4 +1,7 @@
 """
+### old code from first couple tutorials
+###
+
 # base import
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -39,14 +42,46 @@ class HelloTemplate(TemplateView):
 
 from django.shortcuts import render_to_response
 from article.models import Article
+from django.http import HttpResponse
 
 
 def articles(request):
+
+    language = "en-us"
+    session_language = "en-us"
+
+    if 'lang' in request.COOKIES:
+        language = request.COOKIES['lang']
+
+    if 'lang' in request.session:
+        session_language = request.session['lang']
+
     return render_to_response('articles.html',
-        {'articles': Article.objects.all() })
+        {'articles': Article.objects.all(),
+         'language': language,
+         'session_language': session_language
+         })
 
 
 def article(request, article_id=1):
     return render_to_response('article.html',
         {'article': Article.objects.get(id=article_id) })
+
+
+def language(request, language='en-us'):
+    response = HttpResponse('setting language to %s' % language)
+
+    # set cookie var
+    response.set_cookie('lang', language)
+
+    # set session var
+    request.session['lang'] = language
+
+    return response
+
+
+
+
+
+
 
